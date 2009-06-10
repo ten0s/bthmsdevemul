cf9b8752d94b64167f92a7710086f64d7b00102a
dm.klionsky <dm.klionsky@45db8efe-cab1-11dd-86c2-0f8696b7b6f9>
2009-06-10 08:42:17 +0000

    committed 0.9.5 version

--- src/build/build_all.cmd
+++ src/build/build_all.cmd
@@ -5,8 +5,8 @@ set CONFIGURATION=%1
 pushd ..\BthEmulManager
 devenv BthEmulManager.sln /build "%CONFIGURATION%" /project fbtlib /projectconfig "%CONFIGURATION%|Win32"
 devenv BthEmulManager.sln /build "%CONFIGURATION%" /project fbtrt /projectconfig "%CONFIGURATION%|Win32"
-devenv BthEmulManager.sln /build "%CONFIGURATION%" /project bthemul /projectconfig "%CONFIGURATION%|STANDARDSDK_500 (ARMV4I)"
-devenv BthEmulManager.sln /build "%CONFIGURATION%" /project bthemulcom /projectconfig "%CONFIGURATION%|STANDARDSDK_500 (ARMV4I)"
-devenv BthEmulManager.sln /build "%CONFIGURATION%" /project BthEmulAgent /projectconfig "%CONFIGURATION%|STANDARDSDK_500 (ARMV4I)"
+devenv BthEmulManager.sln /build "%CONFIGURATION%" /project bthemul /projectconfig "%CONFIGURATION%|Windows Mobile 5.0 Pocket PC SDK (ARMV4I)"
+devenv BthEmulManager.sln /build "%CONFIGURATION%" /project bthemulcom /projectconfig "%CONFIGURATION%|Windows Mobile 5.0 Pocket PC SDK (ARMV4I)"
+devenv BthEmulManager.sln /build "%CONFIGURATION%" /project BthEmulAgent /projectconfig "%CONFIGURATION%|Windows Mobile 5.0 Pocket PC SDK (ARMV4I)"
 devenv BthEmulManager.sln /build "%CONFIGURATION%" /project BthEmulManagerPlugin /projectconfig "%CONFIGURATION%|Any CPU"
 popd
